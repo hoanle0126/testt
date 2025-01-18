12e31f1559f93f19d4fca98ef3341da7897915b9
hoanle0126 <hoanle0126@gmail.com>
2025-01-18 21:14:07 +0700

    a

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array|string|null
+     */
+    protected $proxies = '*'; // Có thể thay '*' bằng danh sách IP cụ thể nếu cần.
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+}
